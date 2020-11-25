b946c6bb50767f6dbcb48b46491056ab5c173a76
Maremar_Samara <47077099+MaremarSamara@users.noreply.github.com>
2020-11-25 13:04:23 +0200

    creat_project

---
+++ maremar.cs
@@ -0,0 +1,12 @@
+public class animal{
+  string name ="cat";
+ string color="white";
+    
+    
+      static void Main(string[] args)
+  {
+animal cat = new animal();
+    Console.WriteLine(cat.color);
+  }
+
+}
