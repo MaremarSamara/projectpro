6b2a095edffc7576a85b6e2b8407c5382c497870
unknown <marwaana195@gmail.com>
2020-11-25 13:19:47 +0200

    merge remote branch

---
+++ marwa.cs
@@ -0,0 +1,25 @@
+public class jeb {
+
+string name = RangRover;
+
+int idnumber =1999 ;
+
+string color = black ;
+
+float speed = 130 ;
+
+
+}
+
+static void Main(string[] args)
+    {
+
+  jeb rangg = new jeb () ;
+      Console.WriteLine(rangg.name");    
+
+
+
+
+    }
+  }
+}
